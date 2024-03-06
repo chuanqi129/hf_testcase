573bcb40e73355c75740d90f57a878bd18267537
WeizhuoZhang-intel <weizhuo.zhang@intel.com>
2024-03-06 21:33:55 +0800

    add LOG_DIR for run_cpu.log (#1)

--- run_cpu.sh
+++ run_cpu.sh
@@ -1,4 +1,9 @@
-./run_all_task_cpu.sh --model_dtype bfloat16 --warm_up_steps 10 --run_steps 10 > cpu_output_OOB.log 2>&1
-./run_all_task_cpu.sh --model_dtype bfloat16 --warm_up_steps 10 --run_steps 10 --torch_compile True > cpu_output_compile.log 2>&1
+LOG_DIR=${LOG_DIR:-'hf_oob_log'}
+if [ ! -d ${LOG_DIR} ]; then
+    mkdir -p ${LOG_DIR}
+fi
 
-python analyse_logs.py --file_names cpu_output_OOB.log,cpu_output_compile.log --out_name cpu_benchmark.log
+./run_all_task_cpu.sh --model_dtype bfloat16 --warm_up_steps 10 --run_steps 10 > ${LOG_DIR}/cpu_output_OOB.log 2>&1
+./run_all_task_cpu.sh --model_dtype bfloat16 --warm_up_steps 10 --run_steps 10 --torch_compile True > ${LOG_DIR}/cpu_output_compile.log 2>&1
+
+python analyse_logs.py --file_names ${LOG_DIR}/cpu_output_OOB.log,${LOG_DIR}/cpu_output_compile.log --out_name ${LOG_DIR}/cpu_benchmark.log
